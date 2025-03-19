45e15399bd283e0c7aec4b2ea4a5343df5644c6e
Ankit Jha <ankit6686510@gmail.com>
2025-03-19 20:16:09 +0530

    strings

---
+++ second_class/intro.java
@@ -0,0 +1,33 @@
+
+public class intro {
+    public static void main(String[] args) {
+    String str = "my Name is ANkit";
+    String str1 = "HELLO";
+
+    // System.out.println(str.indexOf("N"));
+    // System.out.println(str.lastIndexOf('N'));
+
+    // System.out.println(str.startsWith("my"));
+    // System.out.println(str.endsWith("Ankit"));
+    // // System.out.println(str.replace("my", "i"));
+    // System.out.println(str.contains("hi"));
+    // // System.out.println(str.trim());
+    // // System.out.println(str.substring(1,3));
+    // // System.out.println(str.charAt(2));
+    // // System.out.println(str.equals(str1));
+
+    // // System.out.println("concat :"+str.concat(str1));
+    // // // String str3 = str.concat(str1);
+    // // // System.out.print(str3);
+    // // // str.length();
+    // // System.out.println(str.length());
+    // // // str.toUpperCase()
+    // // System.out.println(str.toUpperCase());
+    // // System.out.println(str.toLowerCase());
+
+    // // System.out.println(str.isEmpty());
+
+    }
+    
+
+}
